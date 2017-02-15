17a65606a5d87e5085f95910bd332372d638b342
Chad Wagner <chawagne@gmail.com>
2017-02-14 19:34:29 -0700

    complete

--- main.js
+++ main.js
@@ -11,42 +11,60 @@
 // Add your JS here.
 
 
-$(document).ready(function() {  
-    
-    
-//1. 
-   
-   
-//2.  
-    
-    
-//3. 
-   
-	    
+$(document).ready(function() {
+
+
+//1.
+$(".alertme").click(function(){
+  alert("jQuery seems easy so far!");
+});
+
+//2.
+// $(".clickme").click(function (){
+//   $(".clickme").text("I was clicked");
+// });
+
+$(".clickme").on("click", function(){
+  $(".clickme").text("I was clicked");
+})
+
+//3.
+$(".addStyle").on("click", function(){
+  $("button").addClass("buttonStyle");
+});
+
 //4.
- 
-   
-//5. 
+$(".addDifferentStyle").click(function () {
+  $(".addDifferentStyle").addClass("addUniqueStyle");
+});
 
-	
+//5.
+$(".removeBtn").click(function () {
+  $("button").removeClass("buttonStyle addUniqueStyle");
+});
+
+
 //6.
-	
-	
+$(".clickToggle").click(function () {
+  $(".clickToggle").toggleClass("buttonStyle");
+})
+
 //7.
+$(".squareBtn").click(function functionName() {
+  $(".square").hide();
+})
 
-	
-	
+
 //8.
-
+$(".circle").click(function functionName() {
+  $(".circle").hide();
+})
 
 
 //9.
+$(".toggleRedSquare").on("click", function () {
+  $(".redSquare").toggle(1500);
+})
 
 
-
-});  // Close: $(document).ready(function() { 
-	
-	
-
-	
-	
+});  // Close: $(document).ready(function() {
